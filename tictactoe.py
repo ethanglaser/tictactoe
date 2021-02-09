20e1500c80f9e58288b9c649bc701e1c2fffdebb
Caleb Samuel <CalebSamuel@Jennifers-MacBook-Pro.local>
2021-02-08 20:04:02 -0600

    My Code

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,3 @@
-
 def displayGrid(info):
     print('   0 1 2')
     print('   _ _ _ ')
@@ -8,38 +7,33 @@ def displayGrid(info):
     print('1 |_|_|_|')
     print('  |' + info[2][0] + '|' + info[2][1] + '|' + info[2][2] + '|')
     print('2 |_|_|_|')
-    print("ayo")
 
 
+def newimput(info):
+    where = input("Player 1, Where would you like to go?")
+    where = where.split(",")
+    xcord = int(where[0])
+    ycord = int(where[1])
+    info[xcord][ycord] = "x"
+    displayGrid(info)
+
+
+    return
+
+def newimput2(info):
+    where = input("Player 2, Where would you like to go?")
+    where = where.split(",")
+    xcord = int(where[0])
+    ycord = int(where[1])
+    info[xcord][ycord] = "o"
+    displayGrid(info)
+
+    return
 
 if __name__ == "__main__":
     info = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']] 
     displayGrid(info)
-
-    # Use the input() function to take in the choices of each player
-    # ex: coordinates = input("Player 1, which coordinates would you like to place an X?")
-    # Remember that all input comes in strings, use int() to convert to an integer
-    # Check the array each move to see if there is a 3 in a row
-    # Check the array to make sure each inputted coordinate is not already occupied
-
-
-
-
-
-
-# Tic Tac Toe
-# 2 players
-# players take turns, one after the other
-# 3x3 grid
-# X's and O's
-# Choose a space that hasn't already been chosen
-# Aim is to get 3 in a row - vertical, horizontal, or diagonal
-# Game ends when a player wins or all spaces are filled
-
-# Code considerations
-# need to get input (can assume both players on same computer)
-# need to display the grid
-# need to track whose turn it is
-# need to determine if chosen space is open
-# need to check if the game is over/if a player has won
-# need to repeat this process if game not over
+    for value in range(0,8):
+        newimput(info)
+        newimput2(info)
+        displayGrid(info)
