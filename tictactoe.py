411776ca27f4f55ec126536560482c98969aa89b
EpicCodingMaster <EyresMac@Jeffreys-iMac.local>
2021-02-08 20:03:44 -0600

    added joyous code

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,3 @@
-
 def displayGrid(info):
     print('   0 1 2')
     print('   _ _ _ ')
@@ -9,36 +8,39 @@ def displayGrid(info):
     print('  |' + info[2][0] + '|' + info[2][1] + '|' + info[2][2] + '|')
     print('2 |_|_|_|')
 
+def game():
+    moves = 1
+    while moves < 9:
+        if moves % 2 == 1:
+            coords = input('Competitor 1 make your decicion in the name of Lithuania: ')
+            splitcoords = coords.split(',')
+            xcoord = int(splitcoords[0])
+            ycoord = int(splitcoords[1])
+            info[ycoord][xcoord] = 'X'
+            displayGrid(info)
+            moves += 1
+        elif moves % 2 == 0:
+            coords2 = input('Competitor 2 make your decision. You do not have the great Blessing of Lithuania: ')
+            splitcoords2 = coords2.split(',')
+            xcoord2 = int(splitcoords2[0])
+            ycoord2 = int(splitcoords2[1])
+            info[ycoord2][xcoord2] = 'O'
+            displayGrid(info)
+
+            moves += 1
+    if moves == 10:
+        print('Joyous game. The great land of Lithuania congratulates the winner. The loser shall go back to the failed state of Estonia')
+
+    return
+    
+    #process the input
+    #check if the move is valid
+    #update list, call displaygrid
+    #check if player has 3 in a row
+    
 
 
 if __name__ == "__main__":
     info = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']] 
     displayGrid(info)
-
-    # Use the input() function to take in the choices of each player
-    # ex: coordinates = input("Player 1, which coordinates would you like to place an X?")
-    # Remember that all input comes in strings, use int() to convert to an integer
-    # Check the array each move to see if there is a 3 in a row
-    # Check the array to make sure each inputted coordinate is not already occupied
-
-
-
-
-
-
-# Tic Tac Toe
-# 2 players
-# players take turns, one after the other
-# 3x3 grid
-# X's and O's
-# Choose a space that hasn't already been chosen
-# Aim is to get 3 in a row - vertical, horizontal, or diagonal
-# Game ends when a player wins or all spaces are filled
-
-# Code considerations
-# need to get input (can assume both players on same computer)
-# need to display the grid
-# need to track whose turn it is
-# need to determine if chosen space is open
-# need to check if the game is over/if a player has won
-#THIS HAS BEEN CLAIMED BY LITHUANIA
+    game()
